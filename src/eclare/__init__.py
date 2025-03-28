50f61e6624def9e03f1b30c27fc51ae7222397a7
Dylan Mann-Krzisnik <dmannk@mcb-gpu1.CS.McGill.CA>
2025-03-28 15:33:37 -0400

    [CKPT] updated loggers using mlflow and run_ECLARE function

--- src/eclare/__init__.py
+++ src/eclare/__init__.py
@@ -6,7 +6,7 @@ from .models import CLIP, load_CLIP_model
 from .setup_utils import return_setup_func_from_dataset, mdd_setup, teachers_setup, merged_dataset_setup
 from .eval_utils import align_metrics, compute_mdd_eval_metrics, foscttm_moscot
 from .data_utils import keep_CREs_and_adult_only, merge_major_cell_group, create_loaders, fetch_data_from_loaders
-from .losses_and_distances_utils import clip_loss, cosine_distance, clip_loss_split_by_ct, Knowledge_distillation_fn, ct_losses
+from .losses_and_distances_utils import clip_loss, cosine_distance, clip_loss_split_by_ct, Knowledge_distillation_fn
 from .run_utils import run_CLIP, save_latents
 from .tune_utils import study_summary
 from .triplet_utils import get_triplet_loss
