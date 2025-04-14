80a83b2e2f75cc066fc2473a4bf6a31eeee2b9d6
Dylan Mann-Krzisnik <dmannk@mcb-gpu1.CS.McGill.CA>
2025-04-14 11:17:40 -0400

    update plot_figures to import model via mlflow

--- scripts/eclare_scripts/eclare_paired_data.sh
+++ scripts/eclare_scripts/eclare_paired_data.sh
@@ -40,7 +40,7 @@ for i in $(seq 0 $((N_CORES - 1))); do
 done
  
 ## Define total number of epochs
-clip_job_id='14080516'
+clip_job_id='13135338'
 total_epochs=100
 
 ## Create a temporary file to store all the commands we want to run
