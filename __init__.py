9ee1da3d9e560846498e0564efcb44ec0970051d
Dylan Mann-Krzisnik <dylan.mann-krzisnik@mail.mcgill.ca>
2025-08-08 13:21:16 -0400

    Simplify __init__ files to only contain set_env_variables

--- __init__.py
+++ __init__.py
@@ -1,11 +1,36 @@
 import os
 import socket
+import sys
 hostname = socket.gethostname()
 
 if hostname.startswith('narval'):
     os.environ['machine'] = 'narval'
 elif hostname == 'MBP-de-Dylan.lan':
     os.environ['machine'] = 'local'
+
+def set_env_variables(config_path='config'):
+    """
+    Set essential environment variables for ECLARE.
+    This function should be called before importing any other ECLARE modules.
+    """
+    # Check if environment variables are already set
+    eclare_root = os.environ.get('ECLARE_ROOT')
+    outpath = os.environ.get('OUTPATH')
+    datapath = os.environ.get('DATAPATH')
+
+    # Print status of environment variables
+    if all([eclare_root, outpath, datapath]):
+        print(f"Environment variables already set:")
+        print(f"ECLARE_ROOT: {eclare_root}")
+        print(f"OUTPATH: {outpath}")
+        print(f"DATAPATH: {datapath}")
+    else:
+        print(f"Missing environment variables")
+
+        sys.path.insert(0, config_path)
+
+        from export_env_variables import export_env_variables
+        export_env_variables(config_path)
 
 '''
 from .triplet_utils import find_mnn_idxs
