13a26cfa80c2a5da28f277934f62798f6b40923d
Dylan Mann-Krzisnik <dylan.mann-krzisnik@mail.mcgill.ca>
2025-08-08 13:21:16 -0400

    Simplify __init__ files to only contain set_env_variables

--- src/eclare/__init__.py
+++ src/eclare/__init__.py
@@ -2,41 +2,39 @@
 eclare: Main package for the ECLARE project.
 """
 
-from .models import CLIP, load_CLIP_model
-from .setup_utils import return_setup_func_from_dataset, mdd_setup, teachers_setup, merged_dataset_setup
-from .eval_utils import align_metrics, compute_mdd_eval_metrics, foscttm_moscot
-from .data_utils import keep_CREs_and_adult_only, merge_major_cell_group, create_loaders, fetch_data_from_loaders
-from .losses_and_distances_utils import clip_loss, cosine_distance, clip_loss_split_by_ct, Knowledge_distillation_fn
-from .run_utils import run_CLIP, save_latents
-from .tune_utils import study_summary
-from .triplet_utils import get_triplet_loss
+import os
+import sys
+
+def set_env_variables(config_path='config'):
+    """
+    Set essential environment variables for ECLARE.
+    This function should be called before importing any other ECLARE modules.
+    """
+    # Check if environment variables are already set
+    eclare_root = os.environ.get('ECLARE_ROOT')
+    outpath = os.environ.get('OUTPATH')
+    datapath = os.environ.get('DATAPATH')
+
+    # Print status of environment variables
+    if all([eclare_root, outpath, datapath]):
+        print(f"Environment variables already set:")
+        print(f"ECLARE_ROOT: {eclare_root}")
+        print(f"OUTPATH: {outpath}")
+        print(f"DATAPATH: {datapath}")
+    else:
+        print(f"Missing environment variables")
+
+        sys.path.insert(0, config_path)
+
+        from export_env_variables import export_env_variables
+        export_env_variables(config_path)
 
 # Define package version
 __version__ = "0.1.0"
 
 # Define public API
 __all__ = [
-    'CLIP',
-    'load_CLIP_model',
-    'return_setup_func_from_dataset',
-    'mdd_setup',
-    'align_metrics',
-    'keep_CREs_and_adult_only',
-    'merge_major_cell_group',
-    'create_loaders',
-    'fetch_data_from_loaders',
-    'clip_loss',
-    'run_CLIP',
-    'study_summary',
-    'cosine_distance',
-    'clip_loss_split_by_ct',
-    'get_triplet_loss',
-    'compute_mdd_eval_metrics',
-    'foscttm_moscot',
-    'Knowledge_distillation_fn',
-    'ct_losses',
-    'save_latents',
-    'teachers_setup'
+    'set_env_variables',
 ]
 
 # Suppress warnings
