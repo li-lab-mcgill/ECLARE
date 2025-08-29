74344ed96d7047a6fcebe681e8710e1fcc80e6ce
Dylan Mann-Krzisnik <dylan.mann-krzisnik@mail.mcgill.ca>
2025-08-29 12:14:05 -0400

    PAGA pseudotime analysis and better subsampling

--- scripts/developmental_post_hoc.py
+++ scripts/developmental_post_hoc.py
@@ -21,7 +21,7 @@ device = torch.device(f'cuda:{n_cudas - 1}') if cuda_available else 'cpu'
 ## Define target and source datasets
 target_dataset = 'Cortex_Velmeshev'
 source_datasets = ['PFC_V1_Wang', 'PFC_Zhu']
-subsample = 20000
+subsample = 2000
 
 ## Create dict for methods and job_ids
 methods_id_dict = {
@@ -139,6 +139,28 @@ student_atac_keep = student_atac[keep_atac].to_memory()
 from copy import deepcopy
 from eclare.post_hoc_utils import create_celltype_palette
 import matplotlib.pyplot as plt
+from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
+
+def subsample_adata(adata, cell_group, dev_group_key, subsample, subsample_type='balanced'):
+    combinations = adata.obs[[cell_group, dev_group_key]].apply(lambda x: ' - '.join(x), axis=1).values
+
+    if subsample_type == 'stratified':
+        n_cells = adata.n_obs
+        n_splits = np.ceil(n_cells / subsample).astype(int)
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=False)
+        _, valid_idx = next(skf.split(np.zeros_like(combinations), combinations))
+
+    elif subsample_type == 'balanced':
+        classes = np.unique(combinations)
+        k = len(classes)
+        per_class = subsample // k
+        counts = Counter(combinations)
+        strategy = {c: min(counts[c], per_class) for c in classes}
+        rus = RandomUnderSampler(random_state=42, sampling_strategy=strategy)
+        valid_idx, valid_devs = rus.fit_resample(np.arange(len(combinations)).reshape(-1, 1), combinations)
+
+    return adata[valid_idx.flatten()].copy()
 
 #student_model = deepcopy(eclare_student_model)
 student_model = deepcopy(kd_clip_student_models['PFC_V1_Wang'])
@@ -150,25 +172,9 @@ student_atac_cells = torch.from_numpy(student_atac_keep.X.toarray().astype(np.fl
 student_rna_latents, _ = student_model.to('cpu')(student_rna_cells, modality=0)
 student_atac_latents, _ = student_model.to('cpu')(student_atac_cells, modality=1)
 
-## subset RNA data for training UMAP
-n_cells = student_rna.n_obs
-n_splits = np.ceil(n_cells / subsample).astype(int)
-
-skf = StratifiedKFold(n_splits=n_splits, shuffle=False)
-combinations = student_rna.obs[[cell_group, dev_group_key]].apply(lambda x: ' - '.join(x), axis=1).values
-_, cells_idx = next(skf.split(np.zeros_like(combinations), combinations))
-
-student_rna_sub = student_rna[cells_idx].copy()
-
-## subset ATAC data for training UMAP
-n_cells = student_atac.n_obs
-n_splits = np.ceil(n_cells / subsample).astype(int)
-
-skf = StratifiedKFold(n_splits=n_splits, shuffle=False)
-combinations = student_atac.obs[[cell_group, dev_group_key]].apply(lambda x: ' - '.join(x), axis=1).values
-_, cells_idx = next(skf.split(np.zeros_like(combinations), combinations))
-
-student_atac_sub = student_atac[cells_idx].copy()
+## subsample data
+student_rna_sub = subsample_adata(student_rna, cell_group, dev_group_key, subsample, subsample_type='stratified')
+student_atac_sub = subsample_adata(student_atac, cell_group, dev_group_key, subsample, subsample_type='stratified')
 
 ## get subset data latents for retraining UMAP
 student_rna_cells_sub = torch.from_numpy(student_rna_sub.X.toarray().astype(np.float32))
@@ -179,63 +185,7 @@ student_atac_latents_sub, _ = student_model.to('cpu')(student_atac_cells_sub, mo
 
 cmap_dev = plt.get_cmap('plasma', len(dev_stages))
 cmap_dev = {dev_stages[i]: cmap_dev(i) for i in range(len(dev_stages))}
-
 cmap_ct = create_celltype_palette(student_rna_sub.obs[cell_group].values, student_atac_sub.obs[cell_group].values, plot_color_palette=False)
-
-
-#%% Retrain UMAP based on original RNA UMAP coordinates
-from umap import UMAP
-import scanpy as sc
-
-def retrain_umap(student_rna_sub):
-    # Use the exact representation used to build neighbors
-    n_pcs = student_rna_sub.obsm['X_pca'].shape[1]
-    X_rep = student_rna_sub.obsm['X_pca'][:, :n_pcs]
-    init_coords = student_rna_sub.obsm['X_umap']                 # from sc.tl.umap
-
-    neigh = student_rna_sub.uns.get('neighbors', {}).get('params', {})
-    umap_uns = student_rna_sub.uns.get('umap', {}).get('params', {})
-
-    mapper = UMAP(
-        n_neighbors=int(neigh.get('n_neighbors', 15)),
-        metric=neigh.get('metric', 'euclidean'),
-        min_dist=float(umap_uns.get('min_dist', 0.5)),
-        spread=float(umap_uns.get('spread', 1.0)),
-        n_components=int(umap_uns.get('n_components', 2)),
-        random_state=umap_uns.get('random_state', 0),
-        init=init_coords,        # “pin” the starting layout to your Scanpy embedding
-    )
-
-    mapper.fit(X_rep)            # now you have a reusable transformer
-    student_rna_sub.obsm['X_umap_retrained'] = mapper.embedding_
-    return mapper
-
-    # For a new dataset:
-    # 1) apply identical preprocessing (same genes/order, normalization, scaling, PCA)
-    # 2) then transform:
-
-mapper = retrain_umap(student_rna_sub)
-
-##
-sc.pp.pca(student_atac_sub, n_comps=n_pcs)
-new_X_rep = student_atac_sub.obsm['X_pca'][:, :n_pcs]
-student_atac_sub.obsm['X_umap_retrained'] = mapper.transform(new_X_rep)
-
-#sc.pl.umap(student_rna_sub, color=cell_group)
-
-## plot RNA umaps
-sc.pl.embedding(student_rna_sub, basis='X_umap', color=cell_group)
-sc.pl.embedding(student_rna_sub, basis='X_umap_retrained', color=cell_group)
-
-student_rna_sub.obs[dev_group_key] = student_rna_sub.obs[dev_group_key].cat.reorder_categories(dev_stages, ordered=True)
-sc.pl.embedding(student_rna_sub, basis='X_umap', color=dev_group_key, palette=cmap_dev, sort_order=True)
-sc.pl.embedding(student_rna_sub, basis='X_umap_retrained', color=dev_group_key, palette=cmap_dev, sort_order=True)
-
-## plot ATAC umaps
-sc.pl.embedding(student_atac_sub, basis='X_umap_retrained', color=cell_group)
-dev_stages_atac = [dev_stage for dev_stage in dev_stages if dev_stage in student_atac_sub.obs[dev_group_key].unique()]
-student_atac_sub.obs[dev_group_key] = student_atac_sub.obs[dev_group_key].cat.reorder_categories(dev_stages_atac, ordered=True)
-sc.pl.embedding(student_atac_sub, basis='X_umap_retrained', color=dev_group_key, palette=cmap_dev, sort_order=True)
 
 
 #%% train UMAP on subsampled data
@@ -266,6 +216,75 @@ else:
 
 #umap_embedder, umap_fig, rna_atac_df_umap = plot_umap_embeddings(rna_latents, atac_latents, rna_dev_stages, atac_dev_stages, rna_condition, atac_condition, cmap_dev)
 umap_embedder, umap_fig, rna_atac_df_umap = plot_umap_embeddings(rna_latents, atac_latents, rna_celltypes, atac_celltypes, rna_condition, atac_condition, cmap_ct)
+
+#%% Create ECLARE adata
+import scanpy as sc
+
+X = np.vstack([
+    student_rna_latents_sub.detach().cpu().numpy(),
+    student_atac_latents_sub.detach().cpu().numpy()
+    ])
+
+obs = pd.concat([
+    student_rna_sub.obs.assign(modality='RNA'),
+    student_atac_sub.obs.assign(modality='ATAC')
+    ])
+
+eclare_adata = sc.AnnData(
+    X=X,
+    obs=obs,
+)
+
+## create unimodal ECLARE adatas
+rna_adata = sc.AnnData(
+    X=student_rna_latents_sub.detach().cpu().numpy(),
+    obs=student_rna_sub.obs.assign(modality='RNA'),
+)
+
+atac_adata = sc.AnnData(
+    X=student_atac_latents_sub.detach().cpu().numpy(),
+    obs=student_atac_sub.obs.assign(modality='ATAC'),
+)
+
+#%% PAGA
+
+def paga_analysis(adata):
+
+    sc.pp.pca(adata)
+    sc.pp.neighbors(adata)
+    sc.tl.leiden(adata, resolution=1.5)
+
+    ## UMAP
+    sc.tl.umap(adata)
+    sc.pl.umap(adata, color=[cell_group, 'modality', dev_group_key])
+
+    #sc.tl.draw_graph(eclare_adata)
+    #sc.pl.draw_graph(eclare_adata, color=[cell_group, 'modality', dev_group_key])
+
+    ## PAGA
+    sc.tl.paga(adata, groups="leiden")
+    sc.pl.paga(adata, color=["leiden", "modality", "Lineage"])
+
+    ## Graph based on PAGA
+    sc.tl.draw_graph(adata, init_pos='paga')
+    sc.pl.draw_graph(adata, color=['Lineage', 'modality', dev_group_key])
+
+    ## Pseudotime with DPT
+    adata.uns['iroot'] = np.flatnonzero(adata.obs[dev_group_key] == '2nd trimester')[0]
+    sc.tl.dpt(adata)
+    sc.pl.draw_graph(adata, color=['dpt_pseudotime', dev_group_key, cell_group, 'modality'])
+
+paga_analysis(eclare_adata)
+
+paga_analysis(rna_adata)
+paga_analysis(atac_adata)
+
+student_rna_sub.obs['modality'] = 'RNA'
+student_atac_sub.obs['modality'] = 'ATAC'
+
+paga_analysis(student_rna_sub)
+paga_analysis(student_atac_sub)
+
 
 #%% project select ATAC and RNA latents through UMAP
 
@@ -324,4 +343,57 @@ cmap_colors = plt.get_cmap('plasma', len(dev_labels.unique()))
 cmap = {dev_labels.unique()[i]: cmap_colors(i) for i in range(len(dev_labels.unique()))}
 phate.plot.scatter2d(phate_op, c=dev_labels, xticklabels=False, yticklabels=False, xlabel='PHATE 1', ylabel='PHATE 2', cmap=cmap)
 
-# %%
+#%% Retrain UMAP based on original RNA UMAP coordinates
+from umap import UMAP
+import scanpy as sc
+
+def retrain_umap(student_rna_sub):
+    # Use the exact representation used to build neighbors
+    n_pcs = student_rna_sub.obsm['X_pca'].shape[1]
+    X_rep = student_rna_sub.obsm['X_pca'][:, :n_pcs]
+    init_coords = student_rna_sub.obsm['X_umap']                 # from sc.tl.umap
+
+    neigh = student_rna_sub.uns.get('neighbors', {}).get('params', {})
+    umap_uns = student_rna_sub.uns.get('umap', {}).get('params', {})
+
+    mapper = UMAP(
+        n_neighbors=int(neigh.get('n_neighbors', 15)),
+        metric=neigh.get('metric', 'euclidean'),
+        min_dist=float(umap_uns.get('min_dist', 0.5)),
+        spread=float(umap_uns.get('spread', 1.0)),
+        n_components=int(umap_uns.get('n_components', 2)),
+        random_state=umap_uns.get('random_state', 0),
+        init=init_coords,        # “pin” the starting layout to your Scanpy embedding
+    )
+
+    mapper.fit(X_rep)            # now you have a reusable transformer
+    student_rna_sub.obsm['X_umap_retrained'] = mapper.embedding_
+    return mapper
+
+    # For a new dataset:
+    # 1) apply identical preprocessing (same genes/order, normalization, scaling, PCA)
+    # 2) then transform:
+
+mapper = retrain_umap(student_rna_sub)
+
+##
+sc.pp.pca(student_atac_sub, n_comps=n_pcs)
+new_X_rep = student_atac_sub.obsm['X_pca'][:, :n_pcs]
+student_atac_sub.obsm['X_umap_retrained'] = mapper.transform(new_X_rep)
+
+#sc.pl.umap(student_rna_sub, color=cell_group)
+
+## plot RNA umaps
+sc.pl.embedding(student_rna_sub, basis='X_umap', color=cell_group)
+sc.pl.embedding(student_rna_sub, basis='X_umap_retrained', color=cell_group)
+
+student_rna_sub.obs[dev_group_key] = student_rna_sub.obs[dev_group_key].cat.reorder_categories(dev_stages, ordered=True)
+sc.pl.embedding(student_rna_sub, basis='X_umap', color=dev_group_key, palette=cmap_dev, sort_order=True)
+sc.pl.embedding(student_rna_sub, basis='X_umap_retrained', color=dev_group_key, palette=cmap_dev, sort_order=True)
+
+## plot ATAC umaps
+sc.pl.embedding(student_atac_sub, basis='X_umap_retrained', color=cell_group)
+dev_stages_atac = [dev_stage for dev_stage in dev_stages if dev_stage in student_atac_sub.obs[dev_group_key].unique()]
+student_atac_sub.obs[dev_group_key] = student_atac_sub.obs[dev_group_key].cat.reorder_categories(dev_stages_atac, ordered=True)
+sc.pl.embedding(student_atac_sub, basis='X_umap_retrained', color=dev_group_key, palette=cmap_dev, sort_order=True)
+
